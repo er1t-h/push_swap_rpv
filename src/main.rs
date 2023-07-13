f56d876953600885077349c2dea591a94c979e7b
hperrin <hperrin@student.42.fr>
2023-07-13 14:54:20 +0200

    Colors on average/median/min/max, handling of bad return

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![warn(clippy::pedantic)]
-#![allow(clippy::cast_precision_loss, clippy::module_name_repetitions)]
+#![allow(
+    clippy::cast_precision_loss,
+    clippy::module_name_repetitions,
+    clippy::cast_possible_truncation,
+    clippy::cast_sign_loss
+)]
 
 mod check_output;
 mod cli;
@@ -29,25 +34,52 @@ const COLORS: [Color; 6] = [
 ];
 
 fn print_correction_criteria(moves: &[usize], thresholds: &[usize], repeat_times: usize) {
+    fn get_threshold_index(thresholds: &[usize], value: usize) -> usize {
+        thresholds.iter().take_while(|&&x| x < value).count()
+    }
     println!();
     println!("Correction Criterias:");
     let thresholds_values = get_threshold_values(moves, thresholds);
     let last_value = thresholds_values[thresholds.len()];
-    for (color, (threshold, value)) in COLORS.iter().zip(thresholds.iter().zip(thresholds_values)) {
+    for (threshold, value) in thresholds.iter().zip(thresholds_values) {
         let to_print = format!(
             "Less than {}: {} ({:.2}%)",
             threshold,
             value,
             value as f64 / repeat_times as f64 * 100.
         );
-        println!("{}", color.paint(to_print));
+        println!("{to_print}");
     }
     let to_print = format!(
         "No points: {} ({:.2}%)",
         last_value,
         last_value as f64 / repeat_times as f64 * 100.
     );
-    println!("{}", COLORS.last().unwrap().paint(to_print));
+    println!("{to_print}");
+
+    let case = *moves.first().unwrap();
+    let threshold_index = get_threshold_index(thresholds, case);
+    let case_str = format!("Best case: {case}");
+    println!("{}", COLORS[threshold_index].paint(case_str));
+
+    let case = moves.iter().sum::<usize>() as f64 / repeat_times as f64;
+    let threshold_index = get_threshold_index(thresholds, case.round() as usize);
+    let case_str = format!("Average case: {case}");
+    println!("{}", COLORS[threshold_index].paint(case_str));
+
+    let case = *moves.last().unwrap();
+    let threshold_index = get_threshold_index(thresholds, case);
+    let case_str = format!("Worst case: {case}");
+    println!("{}", COLORS[threshold_index].paint(case_str));
+
+    let case = if moves.len() % 2 == 1 {
+        moves[moves.len() / 2] as f64
+    } else {
+        (moves[moves.len() / 2] + moves[moves.len() / 2 + 1]) as f64 / 2.
+    };
+    let threshold_index = get_threshold_index(thresholds, case.round() as usize);
+    let case_str = format!("Median case: {case}");
+    println!("{}", COLORS[threshold_index].paint(case_str));
 }
 
 fn main() {
@@ -67,6 +99,10 @@ fn main() {
                 .args(ps_args.iter().map(|x| OsString::from(x.to_string())))
                 .output()
                 .expect("Couldn't run push_swap");
+            if !output.status.success() {
+                eprintln!("Error during run {index}: push_swap returned with a non-zero status");
+                panic!()
+            }
             let output = String::from_utf8(output.stdout).expect("Non UTF-8 push_swap return");
             let has_worked = output::check(ps_args, &output);
             match has_worked {
@@ -80,26 +116,7 @@ fn main() {
         .collect();
     println!("Total time: {:?}", time.elapsed());
     all_moves.sort_unstable();
-    println!("Best case: {}", all_moves.first().unwrap());
-    println!("Worst case: {}", all_moves.last().unwrap());
-    println!(
-        "Average case: {}",
-        all_moves.iter().sum::<usize>() as f64 / args.repeat_times as f64
-    );
-    let median = if all_moves.len() % 2 == 1 {
-        all_moves[all_moves.len() / 2]
-    } else {
-        (all_moves[all_moves.len() / 2] + all_moves[all_moves.len() / 2 + 1]) / 2
-    };
-    println!("Median case: {median}");
-
     match args.number_in_stack {
-        // If you wish to add more thresholds (replace what's between ``):
-        //
-        // `number_in_stack_required` => {
-        //     const THRESHOLDS: [usize; `number_of_thresholds`] = [`your_thresholds`];
-        //     print_correction_criteria(&all_moves, &THRESHOLDS, args.repeat_times);
-        // }
         100 => {
             const THRESHOLDS: [usize; 5] = [700, 900, 1_100, 1_300, 1_500];
             print_correction_criteria(&all_moves, &THRESHOLDS, args.repeat_times);
@@ -108,6 +125,19 @@ fn main() {
             const THRESHOLDS: [usize; 5] = [5_500, 7_000, 8_500, 10_000, 11_500];
             print_correction_criteria(&all_moves, &THRESHOLDS, args.repeat_times);
         }
-        _ => (),
+        _ => {
+            println!("Best case: {}", all_moves.first().unwrap());
+            println!("Worst case: {}", all_moves.last().unwrap());
+            println!(
+                "Average case: {}",
+                all_moves.iter().sum::<usize>() as f64 / args.repeat_times as f64
+            );
+            let median = if all_moves.len() % 2 == 1 {
+                all_moves[all_moves.len() / 2]
+            } else {
+                (all_moves[all_moves.len() / 2] + all_moves[all_moves.len() / 2 + 1]) / 2
+            };
+            println!("Median case: {median}");
+        }
     }
 }
