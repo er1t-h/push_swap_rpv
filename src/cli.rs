899776c6cc37ff37cdf4b1f070dfb4beb9dfc603
hperrin <hperrin@student.42.fr>
2023-07-13 14:54:20 +0200

    Colors on average/median/min/max, handling of bad return

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use clap::Parser;
 
 fn get_default_exec_path() -> PathBuf {
-    let mut path = std::env::current_exe().unwrap();
+    let mut path = std::env::current_dir().unwrap();
     path.pop();
     path.push("push_swap");
     path
@@ -12,7 +12,7 @@ fn get_default_exec_path() -> PathBuf {
 #[derive(Parser)]
 pub struct CliArgs {
     #[arg(long, default_value=get_default_exec_path().into_os_string())]
-    /// The path to the `push_swap` exec. Defaults to `../push_swap`.
+    /// The path to the `push_swap` exec.
     pub path: PathBuf,
     #[arg(long, default_value_t = 500)]
     /// How many numbers will be given to `push_swap`.
